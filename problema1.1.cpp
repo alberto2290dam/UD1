1ce9aca7864ff38816bfac13eb88ea7fca1fb106
Alberto <alberto2290dam@gmail.com>
2015-11-11 22:04:11 +0100

    first commit

---
+++ problema1.1.cpp
@@ -0,0 +1,16 @@
+/*
+Responde verdadero o falso a las siguientes afirmaciones. En cos de que sea falso
+explique brevemente la razon
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+return 0;	
+}
+
+
+
+
